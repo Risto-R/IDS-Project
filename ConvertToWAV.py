1d2303e17205e46e747da14f481aad5dbc84167d
Raud0 <frederikraud1999@gmail.com>
2019-12-13 22:04:57 +0200

    changed some pathnames in notebook, so be carteful

--- ConvertToWAV.py
+++ ConvertToWAV.py
@@ -1,13 +1,20 @@
 import pydub
+import ffmpeg
 import os
-mp3_folder = "Audio_MP3" # Location of mp3 audio files
-wav_folder = "Audio_WAV/" # Location where wav files will be saved
-# Path to FFMPEG bin
-os.environ["PATH"] += os.pathsep + 'FFMPEG/bin'
+mp3_folder = "data/clips/" # Location of mp3 audio files
+wav_folder = "data/wav_clips/" # Location where wav files will be saved
+os.environ["PATH"] += os.pathsep + 'FFMPEG/bin' # Path to FFMPEG bin
 
 dir = os.fsdecode(mp3_folder)
+i = 0
 for path in os.listdir(dir):
-    src = os.path.abspath(os.path.join(dir, path))
-    dst = wav_folder + path[:-3] + str("wav")
-    sound = pydub.AudioSegment.from_mp3(src)
-    sound.export(dst, format("wav"))
+    try:
+        src = os.path.abspath(os.path.join(dir, path))
+        dst = wav_folder + path[:-3] + str("wav")
+        sound = pydub.AudioSegment.from_mp3(src)
+        sound.export(dst, format("wav"))
+    except:
+        print("Broken file " + path)
+    if i % 1000 == 0:
+        print(i)
+    i += 1
