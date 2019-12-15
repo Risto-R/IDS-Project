93ef844a0f1342c9e8e3f0cbdc756f3dc96f0fce
Raud0 <frederikraud1999@gmail.com>
2019-12-15 22:00:17 +0200

    dataset loading functionality

--- ConvertToWAV.py
+++ ConvertToWAV.py
@@ -6,15 +6,13 @@ wav_folder = "data/wav_clips/" # Location where wav files will be saved
 os.environ["PATH"] += os.pathsep + 'FFMPEG/bin' # Path to FFMPEG bin
 
 dir = os.fsdecode(mp3_folder)
-i = 0
 for path in os.listdir(dir):
     try:
         src = os.path.abspath(os.path.join(dir, path))
         dst = wav_folder + path[:-3] + str("wav")
         sound = pydub.AudioSegment.from_mp3(src)
         sound.export(dst, format("wav"))
+        os.remove(src)
     except:
         print("Broken file " + path)
-    if i % 1000 == 0:
-        print(i)
-    i += 1
+
